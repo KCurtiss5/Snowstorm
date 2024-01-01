4f7ff7f30f9f91103f5242206b7ddc15c47242a5
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-01-01 00:03:22 -0700

    Separated colors into its own file

---
+++ src/colors.py
@@ -0,0 +1,17 @@
+colors = {
+    'white': ['\033[37m', '\033[97m'],
+    'rg': [
+        '\033[31m', '\033[32m',  # red, green
+        '\033[91m', '\033[92m',  # bright red, bright green
+           ],
+    'all': [
+        '\033[31m', '\033[32m',  # red, green
+        '\033[33m', '\033[34m',  # yellow & blue
+        '\033[35m', '\033[36m',  # magenta & cyan
+        '\033[37m',              # white
+        '\033[90m', '\033[91m',  # dark grey (bright black) & bright red
+        '\033[92m', '\033[93m',  # bright green & bright yellow
+        '\033[94m', '\033[95m',  # bright blue & bright magenta
+        '\033[96m', '\033[97m',  # bright cyan & bright white
+    ]
+}
--- src/custom_types.py
+++ src/custom_types.py
@@ -1,35 +1,20 @@
-import argparse
-
-colors = {
-    'rg': ['\033[91m', '\033[92m'],  # bright red, bright green
-    'all': [
-        '\033[31m', '\033[32m',  # red, green
-        '\033[33m', '\033[34m',  # yellow & blue
-        '\033[35m', '\033[36m',  # magenta & cyan
-        '\033[37m',              # white
-        '\033[90m', '\033[91m',  # dark grey (bright black) & bright red
-        '\033[92m', '\033[93m',  # bright green & bright yellow
-        '\033[94m', '\033[95m',  # bright blue & bright magenta
-        '\033[96m', '\033[97m',  # bright cyan & bright white
-        # Add more colors as needed
-    ]
-}
+from argparse import ArgumentTypeError
 
 
-def my_type(num, type, min, max=None):
+def _my_type(num, type, min, max=None):
     try:
         num = type(num)
     except ValueError:
-        raise argparse.ArgumentTypeError(f"Must be a {type}.")
+        raise ArgumentTypeError(f"Must be a {type}.")
     if num < min or num > max if max else False:
-        raise argparse.ArgumentTypeError(
+        raise ArgumentTypeError(
             f"Argument must be a {type} between {min} and {max}")
     return num
 
 
 def positive_float(arg):
-    return my_type(arg, float, 0, 10)
+    return _my_type(arg, float, 0, 10)
 
 
 def percentage(arg):
-    return my_type(arg, int, 0, 100)
+    return _my_type(arg, int, 0, 100)
