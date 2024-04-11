ef93e7d85f09a22e17e0ebb6d6bb9f9e9a6d5b99
Koby Curtiss <45833998+KCurtiss5@users.noreply.github.com>
2024-04-10 21:26:59 -0600

    implemented stat string

--- src/helper.py
+++ src/helper.py
@@ -1,2 +1,12 @@
 def clamp(n, minimum, maximum):
     return (max(minimum, min(n, maximum)))
+
+
+def count_new_snowflakes(width, new_row):
+    return width - list.count(new_row, ' ')
+
+def count_snowflakes(width, grid):
+    total = 0
+    for line in grid:
+        total += width - list.count(line, ' ')
+    return total
--- src/snowstorm.py
+++ src/snowstorm.py
@@ -2,17 +2,20 @@ import argparse
 import os
 import random
 import time
+import math
 from custom_types import percentage, positive
 from colors import COLORS
-from helper import clamp
+from helper import clamp, count_new_snowflakes, count_snowflakes
 
 #constants
 RESET = '\033[0m'
+SNOWFLAKES = ['❅', '❆', '❃', '❈', '❉', '*', '•', '·']
+START_TIME = time.time()
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
         prog='Snowstorm',
-        description='snowstorm - create a local snowstorm in your terminal',
+        description='snowstorm - create a snowstorm in your terminal',
     )
     parser.add_argument('-d', '--density', default=5, type=percentage,
         help='Set the percentage of the screen to be snWowflakes. Default: 5')
@@ -24,27 +27,41 @@ def parse_arguments():
         help='Enable wind. The wind direction is random and can change in runtime. Defailt: False')
     parser.add_argument('-a', '--accumulate', nargs='?', default=False, const=5, type=percentage,
         help='Snowflakes have a NUM percent chance to accumulate. Default: False or 5')
+    parser.add_argument('-v', '--statistics', action='store_true',
+        help='Enable program statistics. Default = False')
     args = parser.parse_args()
-    return (args.density, args.delay, args.color, args.wind, args.accumulate)
+    return (args.density, args.delay, args.color,
+            args.wind, args.accumulate, args.statistics)
 
 
-def draw_snowflakes(grid, height):
-    output = ''
-    for row in grid:
-        output += ''.join(row) + '\n'
-    output = output.strip('\n')
+def draw_snowflakes(grid, height, stats_str=None):
+    output = '\n'.join(''.join(row) for row in grid)
     print(output, end='')
+    if stats_str:
+        print(stats_str, end='\033[F')
     print('\033[F' * height, end='')
 
 
 def add_snowflake(density, color):
-    snowflakes = ['❅', '❆', '❃', '❈', '❉', '*', '•', '·']
-    return add_color(random.choice(snowflakes), color) if random.random() < density/100 else ' '
+    return add_color(random.choice(SNOWFLAKES), color) if random.random() < density/100 else ' '
 
 
 def add_color(snowflake, color):
     color = random.choice(COLORS[color])
     return color + snowflake + RESET
+
+def make_stats_str(width, tot_snowflakes, cur_snowflakes, num_frames, runtime) -> str:
+    tot_snow = f"Total Snowflakes: {tot_snowflakes}"
+    cur_snow = f"Current Snowflakes: {cur_snowflakes}"
+    frames = f"Total Frames: {num_frames}"
+    time_str = f"Runtime: {runtime} seconds"
+
+    # Calculate available space for aligning components
+    total_length = sum(map(len, [tot_snow, cur_snow, frames, time_str]))
+    available_space = width - total_length
+    spaces, extra = ' ' * (available_space//3), ' ' * (available_space % 3)
+
+    return tot_snow + spaces + cur_snow + spaces + frames + spaces + time_str + extra
 
 
 def add_wind(grid, strength, num_rows, density, color):
@@ -60,13 +77,20 @@ def add_wind(grid, strength, num_rows, density, color):
         else:
             grid[i] = grid[i][strength:] + snowflakes
 
-
 def main():
-    density, delay, color, wind, accumulate = parse_arguments()
+    density, delay, color, wind, accumulate, stats = parse_arguments()
     width, height = os.get_terminal_size()
+
+    if stats:
+        height -= 1
 
     wind_strength = random.randint(-3, 3)
     rows_with_snow = 0
+
+    #stats variables
+    total_snowflakes = 0
+    stats_str = None
+    num_frames = 0
 
     grid = [[' ' for _ in range(width)] for _ in range(height)]
 
@@ -75,8 +99,14 @@ def main():
 
         for _ in range(width):
             row.append(add_snowflake(density, color))
+
         rows_with_snow += 1
         rows_with_snow = clamp(rows_with_snow, 0, height)
+        if stats:
+            total_snowflakes += count_new_snowflakes(width, row)
+            stats_str = make_stats_str(
+                width, total_snowflakes, count_snowflakes(width, grid), num_frames,
+                math.floor(time.time() - START_TIME))
 
         if wind:
             wind_strength += random.randint(-1, 1)
@@ -88,8 +118,9 @@ def main():
 
         grid.insert(0, row)
         grid.pop()
-        draw_snowflakes(grid, height)
+        draw_snowflakes(grid, height, stats_str)
 
+        num_frames += 1
         time.sleep(delay)
 
 
